d916286a1f48bd97d5abbc32dad1b29ba6cb8a8b
oxxo <oxxo.studio@gmail.com>
2015-04-23 16:25:26 +0800

    add led example

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,7 +1,9 @@
 $(function(){
 	var $window = $(window),
+			$pre = $('pre'),
 			$youtubeIframe = $('.youtube');
 
+	$pre.addClass('prettyprint');
 	
 	var youtubeWidth = $youtubeIframe.width();
 	$youtubeIframe.css({'height':(youtubeWidth*9/16)+'px'});
